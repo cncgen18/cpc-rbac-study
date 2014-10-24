e07b4ddad62f90e7a934ccbd1eddbed1522de386
Newbourne <brian.bland@live.com>
2014-10-23 21:48:59 -0400

    Bland Refactor.

--- web/lib/index.js
+++ web/lib/index.js
@@ -1,17 +1,25 @@
-var Hoek = require('hoek');
-var routes = require('./routes').routes;
+var routes = require('./routes');
+
+var businessOptions = {
+    views: {
+        engines: {
+            handlebars: require('handlebars')
+        },
+        basePath: __dirname,
+        path: './public/templates',
+        compileOptions: {
+            pretty: true
+        }
+    }
+};
 
 exports.register = function (plugin, options, next) {
-    if (typeof options.route != 'undefined'){
-        plugin.config.route.prefix = options.route.prefix;
-    }
-
+    plugin.views(businessOptions.views);
     plugin.route(routes);
-
     next();
 };
 
 exports.register.attributes = {
-    name: 'proto-rbac',
+    name: 'rbac-study',
     version: '1.0.0'
 };
--- web/lib/routes/default.js
+++ web/lib/routes/default.js
@@ -1,3 +1,14 @@
 module.exports = [
+    {
+        method: 'GET',
+        path: '/{param*}',
+        config:{
+            handler: {
+                directory: {
+                    path: __dirname + '../../../public'
+                }
+            }
+        }
+    },
     { method: 'GET', path: '/', handler: { view: 'index.html' } }
 ];
